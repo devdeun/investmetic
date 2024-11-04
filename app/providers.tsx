8fdfd1f3ed17860bc68ebea4248ad77bd3856a2e
James <dawn147@snu.ac.kr>
2024-11-04 18:27:08 +0900

    feat: TanStack Query 세팅 (#6)

---
+++ app/providers.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactNode } from 'react'
+import { getQueryClient } from './get-query-client'
+
+export const Providers = ({ children }: { children: ReactNode }) => {
+  const queryClient = getQueryClient()
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+}
