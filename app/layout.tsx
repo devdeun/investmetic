c440f4d5a71a9dc0c199d3c999ccc85795f859c2
hoyoen <dustmqaksdltkfrlfdldi@gmail.com>
2024-11-08 20:27:48 +0900

    settings: msw 기본 설정 (#18)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next'
 
+import initMSW from '@/mocks'
+
+import { ENV } from '@/shared/constants/env'
 import { QueryProvider } from '@/shared/providers'
 import '@/shared/styles/global.scss'
 
@@ -8,6 +11,10 @@ import { pretendard } from './fonts'
 export const metadata: Metadata = {
   title: 'InvestMetic',
   description: '',
+}
+
+if (process.env.NODE_ENV === ENV.DEVELOPMENT) {
+  initMSW()
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
