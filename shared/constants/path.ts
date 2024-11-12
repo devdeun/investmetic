95c79c656750572533325c867127cba20bf6a803
deun <devdeun@gmail.com>
2024-11-12 22:31:24 +0900

    feat: 라우트 경로 상수 추가 (#23)

---
+++ shared/constants/path.ts
@@ -0,0 +1,24 @@
+export const PATH = {
+  // Auth
+  SIGN_IN: '/signin',
+  SIGN_UP: '/signup',
+
+  // Main
+  HOME: '/',
+  STRATEGIES: '/strategies',
+  TRADERS: '/traders',
+
+  // My
+  FAVORITES: '/my/favorites',
+  MY_STRATEGIES: '/my/strategies',
+  STRATEGIES_MANAGE: '/my/strategies/manage',
+  MY_QUESTIONS: 'my/questions',
+
+  // Admin
+  ADMIN: '/admin',
+  ADMIN_CATEGORY: '/admin/category',
+  ADMIN_USERS: '/admin/users',
+  ADMIN_NOTICES: '/admin/notices',
+  ADMIN_STRATEGIES: '/admin/strategies',
+  ADMIN_QUESTIONS: '/admin/questions',
+} as const
