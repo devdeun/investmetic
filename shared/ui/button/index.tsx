ce453fa26da0bc7f07ea5d8c7c0480e353d5d070
James <dawn147@snu.ac.kr>
2024-11-13 12:51:25 +0900

    feat: 버튼 컴포넌트 추가 (#28)

---
+++ shared/ui/button/index.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { ComponentProps } from 'react'
+
+import classNames from 'classnames/bind'
+
+import styles from './button.module.scss'
+
+const cx = classNames.bind(styles)
+
+type ButtonSizeType = 'small' | 'medium' | 'large'
+type ButtonVariantType = 'outline' | 'filled'
+
+interface ButtonProps extends ComponentProps<'button'> {
+  size: ButtonSizeType
+  variant?: ButtonVariantType
+}
+
+export const Button = ({
+  children,
+  size,
+  variant = 'outline',
+  disabled = false,
+  className,
+  ...props
+}: ButtonProps) => (
+  <button
+    className={cx('button', size, variant, { disabled }, className)}
+    disabled={disabled}
+    {...props}
+  >
+    {children}
+  </button>
+)
