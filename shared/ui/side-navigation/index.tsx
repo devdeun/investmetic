f36171f0b6eb69344a2846447e1371cf3970add0
deun <devdeun@gmail.com>
2024-11-14 12:04:51 +0900

    refactor: NavHeader 컴포넌트 분리 (#23)

--- shared/ui/side-navigation/index.tsx
+++ shared/ui/side-navigation/index.tsx
@@ -1,10 +1,6 @@
-import Link from 'next/link'
-
-import Logo from '@/public/images/logo.svg'
-import TextLogo from '@/public/images/text-logo.svg'
 import classNames from 'classnames/bind'
 
-import { PATH } from '@/shared/constants/path'
+import NavHeader from '@/shared/ui/side-navigation/nav-header'
 import UserNavigation from '@/shared/ui/side-navigation/user-navigation'
 
 import styles from './styles.module.scss'
@@ -18,14 +14,7 @@ interface Props {
 const SideNavigation = ({ children }: Props) => {
   return (
     <aside className={cx('aside')}>
-      <header>
-        <h1>
-          <Link href={PATH.HOME} className={cx('logo')}>
-            <Logo />
-            <TextLogo className={cx('text')} aria-label="인베스트메틱" />
-          </Link>
-        </h1>
-      </header>
+      <NavHeader />
 
       <nav className={cx('main-navigation')} aria-label="메인 메뉴">
         <ul>{children}</ul>
