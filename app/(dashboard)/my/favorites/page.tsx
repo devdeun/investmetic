cc92c7f83e3ae2bd2d3c2487ed7e929bfc5d7152
deun <devdeun@gmail.com>
2024-11-15 16:34:15 +0900

    feat: 각 라우트 폴더에 page.tsx 파일 추가 (#35)

---
+++ app/(dashboard)/my/favorites/page.tsx
@@ -0,0 +1,5 @@
+const MyFavoritesPage = () => {
+  return <></>
+}
+
+export default MyFavoritesPage
---
+++ app/admin/category/page.tsx
@@ -0,0 +1,5 @@
+const AdminCategoryPage = () => {
+  return <></>
+}
+
+export default AdminCategoryPage
