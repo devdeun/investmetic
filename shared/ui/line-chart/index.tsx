16917a1cf3c6f463690f9d88cfab3ed3827ca831
James <dawn147@snu.ac.kr>
2024-11-17 17:02:37 +0900

    feat: 라인 차트 컴포넌트 추가 (#52)

---
+++ shared/ui/line-chart/index.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+
+import Highcharts from 'highcharts'
+
+import { CardSizeType } from '../sm-score-card/index'
+
+const HighchartsReact = dynamic(() => import('highcharts-react-official'), {
+  ssr: false,
+})
+
+interface ChartProps {
+  data: number[]
+  isNegative?: boolean
+  size?: CardSizeType
+}
+
+const getChartDimensions = (size: CardSizeType) => ({
+  height: size === 'small' ? 55 : 165,
+  width: size === 'small' ? 90 : 185,
+})
+
+const Chart = ({ data, isNegative = false, size = 'small' }: ChartProps) => {
+  const dimensions = getChartDimensions(size)
+
+  const chartOptions: Highcharts.Options = {
+    chart: {
+      type: 'line',
+      height: dimensions.height,
+      width: dimensions.width,
+      backgroundColor: 'transparent',
+      margin: [0, 0, 0, 0],
+    },
+    title: { text: undefined },
+    xAxis: {
+      visible: false,
+    },
+    yAxis: {
+      visible: false,
+      min: Math.min(...data) * 0.95,
+      max: Math.max(...data) * 1.05,
+    },
+    legend: { enabled: false },
+    plotOptions: {
+      series: {
+        animation: false,
+        lineWidth: 2,
+        marker: { enabled: false },
+        states: { hover: { enabled: false } },
+        enableMouseTracking: false,
+      },
+    },
+    series: [
+      {
+        type: 'line',
+        data,
+        color: isNegative ? '#6877FF' : '#FF7752',
+      },
+    ],
+    credits: { enabled: false },
+    tooltip: { enabled: false },
+  }
+
+  return <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+}
+
+export default Chart
