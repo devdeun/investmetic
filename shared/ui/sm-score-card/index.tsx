13b2f93bdf1822582f2d645cff4e996cd3ce742a
James <dawn147@snu.ac.kr>
2024-11-17 17:05:31 +0900

    feat: sm-score-card 컴포넌트 추가 (#52) - 스토리북도 추가(배경은 검은색으로 설정해놨습니다)

---
+++ shared/ui/sm-score-card/index.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import classNames from 'classnames/bind'
+
+import Avatar from '@/shared/ui/avatar'
+
+import Chart from '../line-chart'
+import styles from './styles.module.scss'
+
+const cx = classNames.bind(styles)
+
+export type CardSizeType = 'small' | 'large'
+
+export interface ScoreCardProps {
+  name: string
+  title: string
+  score: number
+  percentageChange: number
+  chartData: number[]
+  ranking: number
+  profileImage?: string
+  size?: CardSizeType
+}
+
+const ScoreCard = ({
+  name,
+  title,
+  score,
+  percentageChange,
+  chartData,
+  ranking,
+  profileImage,
+  size = 'small',
+}: ScoreCardProps) => {
+  const isNegative = percentageChange < 0
+
+  return (
+    <div className={cx('card-wrapper', size)}>
+      <div className={cx('top-frame', size)}>
+        <div className={cx('content-area')}>
+          <div className={cx('rank')}>Top {ranking}</div>
+          <div className={cx('profile')}>
+            <Avatar src={profileImage} size="small" />
+            <span className={cx('profile-name')}>{name}</span>
+          </div>
+          <h3 className={cx('title')}>{title}</h3>
+        </div>
+        <div className={cx('chart-area', size)}>
+          <Chart data={chartData} isNegative={isNegative} size={size} />
+        </div>
+      </div>
+      <div className={cx('bottom-frame')}>
+        <div className={cx('sm-score')}>
+          <span className={cx('label')}>SM SCORE</span>
+          <span className={cx('value', 'score')}>{score.toFixed(2)}</span>
+        </div>
+        <div className={cx('profit')}>
+          <span className={cx('label')}>누적수익률</span>
+          <span className={cx('value', { negative: isNegative })}>
+            {isNegative ? '' : '+'}
+            {percentageChange}%
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ScoreCard
