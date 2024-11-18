85d8af47467cc34a77e3e7e910c56d89e57d9240
hoyoen <dustmqaksdltkfrlfdldi@gmail.com>
2024-11-19 01:57:31 +0900

    refactor: Header 컴포넌트 구조 수정 (#74)

--- shared/ui/header/index.tsx
+++ shared/ui/header/index.tsx
@@ -1,20 +1,22 @@
+import { CSSProperties, ReactNode } from 'react'
+
 import classNames from 'classnames/bind'
 
-import Logo from '../logo'
-import HeaderLinks from './_ui/header-links'
 import styles from './header.module.scss'
 
 const cx = classNames.bind(styles)
 
 interface Props {
-  hasLinks?: boolean
+  Left?: ReactNode
+  Right?: ReactNode
+  styles?: CSSProperties
 }
 
-const Header = ({ hasLinks = false }: Props) => {
+const Header = ({ Left, Right }: Props) => {
   return (
-    <header className={cx('container')}>
-      <Logo hasText />
-      {hasLinks && <HeaderLinks />}
+    <header className={cx('container')} style={styles}>
+      {Left}
+      {Right}
     </header>
   )
 }
---
+++ shared/ui/header/logo-header/index.tsx
@@ -0,0 +1,25 @@
+import Header from '..'
+import Logo from '../../logo'
+import HeaderLinks from '../_ui/header-links'
+
+const headerStyles = {
+  background: 'rgba(251, 251, 251, 0.5)',
+  backdropFilter: 'blur(60px)',
+}
+
+interface Props {
+  hasLinks?: boolean
+  hasText?: boolean
+}
+
+const LogoHeader = ({ hasLinks = false, hasText = false }: Props) => {
+  return (
+    <Header
+      Left={<Logo hasText={hasText} />}
+      Right={hasLinks && <HeaderLinks />}
+      styles={headerStyles}
+    />
+  )
+}
+
+export default LogoHeader
