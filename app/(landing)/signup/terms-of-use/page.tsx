04277c49d01b968e4b0ee7b5326164c53169edb5
deun <devdeun@gmail.com>
2024-11-20 16:37:36 +0900

    feat: 회원가입 약관 동의 페이지 구현 (#81)

--- app/(landing)/signup/terms-of-use/page.tsx
+++ app/(landing)/signup/terms-of-use/page.tsx
@@ -1,14 +1,99 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import useSignupStore from '@/app/(landing)/signup/_stores/use-signup-store'
 import Step from '@/app/(landing)/signup/_ui/step'
+import TermsContainer from '@/app/(landing)/signup/terms-of-use/_ui/terms-container'
+import classNames from 'classnames/bind'
 
 import { PATH } from '@/shared/constants/path'
+import { Button } from '@/shared/ui/button'
+import Checkbox from '@/shared/ui/check-box'
 import { LinkButton } from '@/shared/ui/link-button'
 
+import InvestorTerms from './_ui/terms/investor-terms'
+import PrivacyTerms from './_ui/terms/privacy-terms'
+import TraderTerms from './_ui/terms/trader-terms'
+import styles from './page.module.scss'
+
+const cx = classNames.bind(styles)
+
 const TermsOfUsePage = () => {
+  const router = useRouter()
+  const userType = useSignupStore((state) => state.userType)
+  const isAgreedTerm = useSignupStore((state) => state.isAgreedTerm)
+  const { setIsAgreedTerm } = useSignupStore((state) => state.actions)
+
+  const [isUserTermChecked, setIsUserTermChecked] = useState(isAgreedTerm)
+  const [isPrivacyTermChecked, setIsPrivacyTermChecked] = useState(isAgreedTerm)
+  const [isAllChecked, setIsAllChecked] = useState(isAgreedTerm)
+
+  useEffect(() => {
+    if (!userType) {
+      router.push(PATH.SIGN_UP_USER_TYPE)
+    }
+  }, [userType, router])
+
+  const handleAllCheck = () => {
+    setIsAllChecked(!isAllChecked)
+    setIsUserTermChecked(!isAllChecked)
+    setIsPrivacyTermChecked(!isAllChecked)
+  }
+
+  const handleUserTermCheck = () => {
+    setIsUserTermChecked(!isUserTermChecked)
+    setIsAllChecked(!isUserTermChecked && isPrivacyTermChecked)
+  }
+
+  const handlePrivacyTermCheck = () => {
+    setIsPrivacyTermChecked(!isPrivacyTermChecked)
+    setIsAllChecked(!isPrivacyTermChecked && isUserTermChecked)
+  }
+
+  const handleNextClick = () => {
+    setIsAgreedTerm(isAllChecked)
+    router.push(PATH.SIGN_UP_INFORMATION)
+  }
+
   return (
     <>
       <Step />
-      <LinkButton href={PATH.SIGN_UP_INFORMATION}>다음</LinkButton>
-      <LinkButton href={PATH.SIGN_UP_USER_TYPE}>이전</LinkButton>
+      <div className={cx('container')}>
+        <div className={cx('all-check-wrapper')}>
+          <Checkbox
+            label=" 이용약관, 개인정보 취급방침에 모두 동의합니다."
+            isChecked={isAllChecked}
+            onChange={handleAllCheck}
+            textColor="gray800"
+            textSize="b2"
+          />
+        </div>
+
+        <TermsContainer
+          title="이용약관"
+          isChecked={isPrivacyTermChecked}
+          onChange={handlePrivacyTermCheck}
+        >
+          <PrivacyTerms />
+        </TermsContainer>
+        <TermsContainer
+          title="개인정보 취급방침"
+          isChecked={isUserTermChecked}
+          onChange={handleUserTermCheck}
+        >
+          {userType === 'trader' ? <TraderTerms /> : <InvestorTerms />}
+        </TermsContainer>
+
+        <div className={cx('button-wrapper')}>
+          <LinkButton href={PATH.SIGN_UP_USER_TYPE}>이전</LinkButton>
+          <Button onClick={handleNextClick} variant="filled" disabled={!isAllChecked}>
+            다음
+          </Button>
+        </div>
+      </div>
     </>
   )
 }
