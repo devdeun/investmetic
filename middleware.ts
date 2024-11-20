fd52087d6cd372cb23c5dc47dd2b7960bfeb52b5
deun <devdeun@gmail.com>
2024-11-20 22:17:39 +0900

    feat: 회원가입 페이지 접근 제어를 위한 미들웨어 추가 (#81)

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+import { handleSignupMiddleware } from '@/middleware/signup'
+
+import { PATH } from '@/shared/constants/path'
+
+export const middleware = (request: NextRequest) => {
+  const path = request.nextUrl.pathname
+
+  if (path.startsWith(PATH.SIGN_UP)) {
+    const response = handleSignupMiddleware(request)
+    if (response) return response
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [`${PATH.SIGN_UP}/:path*`],
+}
