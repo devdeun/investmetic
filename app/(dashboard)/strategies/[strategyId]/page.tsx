5e080f5fd52a7c0ecae11efa6e65beda600a9627
ssumanlife <ksm9805@naver.com>
2024-11-20 13:34:04 +0900

    remove: 예시코드 삭제 (#69)

--- app/(dashboard)/strategies/[strategyId]/page.tsx
+++ app/(dashboard)/strategies/[strategyId]/page.tsx
@@ -1,27 +1,4 @@
-import DetailsSideItem, {
-  TitleType,
-} from '@/app/(dashboard)/strategies/[strategyId]/_ui/details-side-item'
-
 const StrategyDetailPage = () => {
-  const arr1: { title: TitleType; data: string | number }[] = [
-    { title: '최종손익입력일자', data: '2016.04.30' },
-    { title: '등록일', data: '2016.02.30' },
-  ]
-  const arr2: { title: TitleType; data: string | number }[] = [
-    { title: '최종손익입력일자', data: '2016.04.30' },
-    { title: '등록일', data: '2016.02.30' },
-  ]
-  const trader: { title: TitleType; data: string | number } = { title: '트레이더', data: '수밍' }
-  const arr = [trader, arr1, arr2]
-  return (
-    <>
-      {arr.map((i, idx) => (
-        <div key={idx}>
-          <DetailsSideItem information={i} />
-        </div>
-      ))}
-    </>
-  )
+  return <></>
 }
-
 export default StrategyDetailPage
