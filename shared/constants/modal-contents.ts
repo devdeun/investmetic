7c1c854410bd999e67128284a768a297478f86ef
nanafromjeju <nanafromjeju@gmail.com>
2024-11-22 05:33:29 +0900

    feat: Modal 내용 상수 추가 (#59)

--- shared/constants/modal-contents.ts
+++ shared/constants/modal-contents.ts
@@ -1,6 +1,11 @@
 export const MODAL_CONTENTS = {
   SUBSCRIBE: '전략을 구독합니다. \n 구독한 전략은 나의 관심전략 \n 페이지에서 확인 가능합니다.',
-  VERIFICATIONCODE: '인증코드가 이메일로 발송되었습니다.\n 메일을 확인해주세요.',
+  VERIFICATION_CODE: '인증코드가 이메일로 발송되었습니다.\n 메일을 확인해주세요.',
   LOGIN: '로그인이 필요합니다.\n 로그인하시겠습니까?',
   DELETE: '리뷰를 삭제하겠습니까?',
+  CANCEL: '구독을 취소하시겠습니까?',
+  TRADE_UPDATE: '매매유형 수정',
+  TRADE_CREATE: '매매유형 등록',
+  ITEM_UPDATE: '종목 수정',
+  ITEM_CREATE: '종목 등록',
 }
