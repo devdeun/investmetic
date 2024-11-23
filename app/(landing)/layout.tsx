ce320a2598cf0c7f79142071771a5f2132228e59
James <dawn147@snu.ac.kr>
2024-11-23 16:16:28 +0900

    feat: 랜딩 디렉토리 헤더 추가 (#78)

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,13 +1,27 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+import LogoHeader from '@/shared/ui/header/logo-header'
+
 interface Props {
   children: React.ReactNode
 }
 
-const HomeLayout = ({ children }: Props) => {
+const LandingLayout = ({ children }: Props) => {
+  const pathname = usePathname()
+
+  const getHeaderType = () => {
+    if (pathname.includes('/signin') || pathname.includes('/signup')) return false
+    return true
+  }
+
   return (
     <>
+      <LogoHeader hasText={true} hasLinks={getHeaderType()} />
       <main className="landing-main">{children}</main>
     </>
   )
 }
 
-export default HomeLayout
+export default LandingLayout
