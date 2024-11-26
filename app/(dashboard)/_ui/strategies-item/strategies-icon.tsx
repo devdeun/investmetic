f64db78f35f81c9bad9c4aea191bfc931cfc21b5
ssumanlife <ksm9805@naver.com>
2024-11-27 01:44:12 +0900

    feat: 종목&매매 아이콘 컴포넌트 추가 (#83)

--- app/(dashboard)/_ui/strategies-item/strategies-icon.tsx
+++ app/(dashboard)/_ui/strategies-item/strategies-icon.tsx
@@ -1,9 +1,75 @@
-interface Props {
-  icon: string[] | React.ElementType
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import Image from 'next/image'
+
+import classNames from 'classnames/bind'
+import { Tooltip } from 'react-tooltip'
+
+import styles from './styles.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface ImageSizeModel {
+  width: number
+  height: number
 }
 
-const StrategiesIcon = ({ icon }: Props) => {
-  return <div>[종목 & 매매 Icon]</div>
+interface Props {
+  iconUrls?: string[]
+  iconNames?: string[]
+}
+
+const StrategiesIcon = ({ iconUrls, iconNames }: Props) => {
+  const [imageSizes, setImageSizes] = useState<{ [key: string]: ImageSizeModel }>({})
+
+  useEffect(() => {
+    iconUrls?.forEach((url) => {
+      const image = new window.Image()
+      image.src = url
+      image.onload = () => handleImageLoad(url, image)
+    })
+  }, [iconUrls])
+
+  const handleImageLoad = (url: string, image: HTMLImageElement) => {
+    setImageSizes((prev) => ({
+      ...prev,
+      [url]: { width: image.width, height: image.height },
+    }))
+  }
+
+  if (iconUrls?.length === 0 || iconNames?.length === 0) return null
+
+  return (
+    <div className={cx('icon-container')}>
+      {iconUrls?.map((url, idx) => {
+        const name = iconNames?.[idx]
+
+        if (!url || !name) return null
+        const size = imageSizes[url] || { width: 20, height: 20 }
+        return (
+          <div key={url} className={cx('icon-wrapper')}>
+            <div
+              className={cx('icon')}
+              style={{
+                width: `${size.width}px`,
+                height: `${size.height}px`,
+              }}
+              data-tooltip-id={name}
+              data-tooltip-content={name}
+              data-tooltip-class-name="tooltip"
+            >
+              <Image src={url} alt={name} fill />
+            </div>
+            <Tooltip id={name} className={cx('tooltip')} classNameArrow={cx('arrow')} opacity={1}>
+              {name}
+            </Tooltip>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default StrategiesIcon
