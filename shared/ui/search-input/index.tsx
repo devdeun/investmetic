935aa04caf95963889d73c2c247644b00bd4c75e
nanafromjeju <nanafromjeju@gmail.com>
2024-11-27 02:22:10 +0900

    feat: SearchInput 컴포넌트 ref 속성 추가 (#126)

--- shared/ui/search-input/index.tsx
+++ shared/ui/search-input/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ComponentProps } from 'react'
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
 
 import { SearchIcon } from '@/public/icons'
 import classNames from 'classnames/bind'
@@ -9,28 +9,27 @@ import styles from './styles.module.scss'
 
 const cx = classNames.bind(styles)
 
-interface Props extends ComponentProps<'input'> {
+interface Props extends ComponentPropsWithoutRef<'input'> {
   placeholder?: string
   handleSearchIconClick?: () => void
 }
 
-export const SearchInput = ({
-  placeholder = '',
-  handleSearchIconClick,
-  value,
-  onChange,
-  ...props
-}: Props) => {
-  return (
-    <div className={cx('search-input-container')}>
-      <input
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        className={cx('search-input')}
-        {...props}
-      />
-      <SearchIcon className={cx('search-icon')} onClick={handleSearchIconClick} />
-    </div>
-  )
-}
+export const SearchInput = forwardRef<HTMLInputElement, Props>(
+  ({ placeholder = '', handleSearchIconClick, value, onChange, ...props }: Props, ref) => {
+    return (
+      <div className={cx('search-input-container')}>
+        <input
+          ref={ref}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          className={cx('search-input')}
+          {...props}
+        />
+        <SearchIcon className={cx('search-icon')} onClick={handleSearchIconClick} />
+      </div>
+    )
+  }
+)
+
+SearchInput.displayName = 'SearchInput'
