2a6ea24fbe28311e4b380124805244684b2c4700
deun <devdeun@gmail.com>
2024-11-27 11:25:11 +0900

    fix: errormessage 컴포넌트에서 에러 메시지를 옵셔널하게 받을 수 있도록 수정 (#135)

--- shared/ui/error-message/index.tsx
+++ shared/ui/error-message/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.scss'
 const cx = classNames.bind(styles)
 
 interface Props {
-  errorMessage: string | null | undefined
+  errorMessage?: string | null
 }
 export const ErrorMessage = ({ errorMessage }: Props) => {
   return <>{errorMessage && <p className={cx('error-message')}>{errorMessage}</p>}</>
