99fc5ba27db4bfbf4d9f44956cb97c58152ec2ec
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-12-01 03:58:08 +0900

    fix: 관리자 매매 유형 inline style 제거 (#162)

--- app/admin/strategies/page.tsx
+++ app/admin/strategies/page.tsx
@@ -1,37 +1,27 @@
 'use client'
 
-// TODO: ssr
 import { useState } from 'react'
+
+import classNames from 'classnames/bind'
 
 import Tabs from '@/shared/ui/tabs'
 import Title from '@/shared/ui/title'
 
 import StockManage from './_ui/stock/stock-manage'
 import TradeManage from './_ui/trade/trade-manage'
+import styles from './page.module.scss'
 
-// import styles from './page.module.scss'
+const cx = classNames.bind(styles)
 
-// const cx = classNames.bind(styles)
-
-const tabs = ['종목', '매매유형']
-
-type TabType = '종목' | '매매유형'
+const tabs = ['종목', '매매유형'] as const
 
 const AdminStrategiesPage = () => {
-  const [activeTab, setActiveTab] = useState<TabType>('종목')
+  const [activeTab, setActiveTab] = useState<(typeof tabs)[number]>('매매유형')
 
   return (
     <>
-      {/* TODO: inline css 제거 */}
       <Title label="종목 및 매매 유형 관리" style={{ margin: '80px 0 26px 12.6px' }} />
-      <div
-        style={{
-          padding: '0 45px 37px',
-          borderRadius: '8px',
-          marginBottom: '42px',
-          backgroundColor: 'aliceblue',
-        }}
-      >
+      <div className={cx('container')}>
         <Tabs
           tabs={tabs.map((tab) => ({
             id: tab,
