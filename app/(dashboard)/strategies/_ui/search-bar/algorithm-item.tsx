0c1eac42838f019117a0906670046f4a68cdd0c3
ssumanlife <ksm9805@naver.com>
2024-12-02 13:26:23 +0900

    feat: searchWord 상태 변경 액션 추가 및 백엔드에서 지정한 검색어 enum값으로 상태 저장 (#181)

--- app/(dashboard)/strategies/_ui/search-bar/algorithm-item.tsx
+++ app/(dashboard)/strategies/_ui/search-bar/algorithm-item.tsx
@@ -8,16 +8,17 @@ import styles from './styles.module.scss'
 const cx = classNames.bind(styles)
 
 interface Props {
-  name: AlgorithmItemType
+  optionId: AlgorithmItemType
+  name: string
   clickedAlgorithm: AlgorithmItemType | null
   onChange: (algorithm: AlgorithmItemType) => void
 }
 
-const AlgorithmItem = ({ name, clickedAlgorithm, onChange }: Props) => {
+const AlgorithmItem = ({ optionId, name, clickedAlgorithm, onChange }: Props) => {
   return (
     <button
-      className={cx('algorithm-button', { active: clickedAlgorithm === name })}
-      onClick={() => onChange(name)}
+      className={cx('algorithm-button', { active: clickedAlgorithm === optionId })}
+      onClick={() => onChange(optionId)}
     >
       {name}
     </button>
