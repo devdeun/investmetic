703c4b14afb306c7db419af30ef4e67ea50c402c
nanafromjeju <nanafromjeju@gmail.com>
2024-12-04 02:44:31 +0900

    feat: MyProfileWithdrawPage 페이지 생성 (#127)

---
+++ app/(dashboard)/my/profile/withdraw/page.tsx
@@ -0,0 +1,11 @@
+import UserWithdraw from '../_ui/user-withdraw'
+
+const MyProfileWithdrawPage = () => {
+  return (
+    <>
+      <UserWithdraw />
+    </>
+  )
+}
+
+export default MyProfileWithdrawPage
