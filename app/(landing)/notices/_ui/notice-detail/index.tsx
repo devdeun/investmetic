c2cf306f8d9103caa9937835d99141c49d2f206a
nanafromjeju <nanafromjeju@gmail.com>
2024-12-05 00:55:19 +0900

    refactor: NoticeDetail 컴포넌트 Props제거 (#217)

--- app/(landing)/notices/_ui/notice-detail/index.tsx
+++ app/(landing)/notices/_ui/notice-detail/index.tsx
@@ -7,65 +7,48 @@ import styles from './styles.module.scss'
 
 const cx = classNames.bind(styles)
 
-interface Props {
-  title: React.ReactNode
-  date: string
-  children?: React.ReactNode
-  hasAttachFile?: boolean
-}
+const NoticeDetail = () => {
+  // TODO: 임시데이터 제거
+  const data = {
+    title: '공지사항 제목입니다.',
+    createdAd: '2024-12-05',
+    content: '이것은 공지사항의 내용입니다. 자세한 사항은 첨부 파일을 확인하세요.',
+    attachments: [
+      { title: '파일제목1.pdf', url: '/path/to/file1.pdf' },
+      { title: '파일제목2.pdf', url: '/path/to/file2.pdf' },
+      { title: '파일제목3.pdf', url: '/path/to/file3.pdf' },
+      { title: '파일제목4.pdf', url: '/path/to/file4.pdf' },
+      { title: '파일제목5.pdf', url: '/path/to/file5.pdf' },
+    ],
+  }
 
-const NoticeDetail = ({ title, date, children, hasAttachFile = true }: Props) => {
   const handleSave = () => {}
+
   return (
     <div className={cx('container')}>
       <div className={cx('title-wrapper')}>
-        <div className={cx('title')}>{title}</div>
-        <div className={cx('date')}>{date}</div>
+        <div className={cx('title')}>{data.title}</div>
+        <div className={cx('date')}>{data.createdAd}</div>
       </div>
       <div className={cx('top-line')}></div>
-      <div className={cx('content')}>{children}</div>
+      <div className={cx('content')}>{data.content}</div>
       <div className={cx('bottom-line')}></div>
 
-      {hasAttachFile && (
-        <div className={cx('attach-file')}>
-          <div className={cx('file-left-wrapper')}>
-            <p className={cx('file-count')}>첨부 {'5'}개</p>{' '}
-          </div>
-          <div className={cx('file-right-wrapper')}>
-            <div className={cx('file-wrapper')}>
-              <div className={cx('file-title')} onClick={handleSave}>
-                {'파일제목1.pdf'}
-              </div>
-              <DownloadIcon className={cx('file-download')} onClick={handleSave} />
-            </div>
-
-            <div className={cx('file-wrapper')}>
-              <div className={cx('file-title')} onClick={handleSave}>
-                {'파일제목2.pdf'}
-              </div>
-              <DownloadIcon className={cx('file-download')} onClick={handleSave} />
-            </div>
-            <div className={cx('file-wrapper')}>
-              <div className={cx('file-title')} onClick={handleSave}>
-                {'파일제목3.pdf'}
-              </div>
-              <DownloadIcon className={cx('file-download')} onClick={handleSave} />
-            </div>
-            <div className={cx('file-wrapper')}>
-              <div className={cx('file-title')} onClick={handleSave}>
-                {'파일제목4.pdf'}
-              </div>
-              <DownloadIcon className={cx('file-download')} onClick={handleSave} />
-            </div>
-            <div className={cx('file-wrapper')}>
-              <div className={cx('file-title')} onClick={handleSave}>
-                {'파일제목5.pdf'}
-              </div>
-              <DownloadIcon className={cx('file-download')} onClick={handleSave} />
-            </div>
-          </div>
+      <div className={cx('attach-file')}>
+        <div className={cx('file-left-wrapper')}>
+          <p className={cx('file-count')}>첨부 {data.attachments.length}개</p>
         </div>
-      )}
+        <div className={cx('file-right-wrapper')}>
+          {data.attachments.map((file, index) => (
+            <div className={cx('file-wrapper')} key={index}>
+              <div className={cx('file-title')} onClick={() => handleSave}>
+                {file.title}
+              </div>
+              <DownloadIcon className={cx('file-download')} onClick={() => handleSave} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
