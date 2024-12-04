1cf49718240e8b1817008caee9447a1c43b89467
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-12-04 12:12:02 +0900

    design: 공지 목록 페이지 디자인 (#90)

--- app/admin/_ui/admin-header/index.tsx
+++ app/admin/_ui/admin-header/index.tsx
@@ -1,31 +1,25 @@
 import { CSSProperties, ReactNode } from 'react'
 
-import Header from '@/shared/ui/header'
-import { SearchInput } from '@/shared/ui/search-input'
+import classNames from 'classnames/bind'
+
+import styles from './styles.module.scss'
+
+const cx = classNames.bind(styles)
 
 interface Props {
   Left?: ReactNode
   Right?: ReactNode
   styles?: CSSProperties
+  className?: string
 }
 
-const headerStyles: CSSProperties = {
-  position: 'static',
-  padding: '0',
-  height: 'calc(40px * 2 + 18px)',
-  // font B3
-  fontSize: '14px',
-  fontWeight: '600',
-  lineHeight: '132%',
-  letterSpacing: '-0.28px',
+const AdminContentsHeader = ({ Left, Right, className }: Props) => {
+  return (
+    <div className={cx('container', className)}>
+      {Left}
+      {Right}
+    </div>
+  )
 }
 
-const AdminContentsHeader = ({ Left, Right }: Props) => {
-  return <Header as="div" Left={Left} Right={Right} styles={headerStyles} />
-}
-
-const Search = () => {
-  return <SearchInput placeholder="제목을 검색하세요." />
-}
-
-export default Object.assign(AdminContentsHeader, { Search })
+export default AdminContentsHeader
