52d64fe602913a59f8db4eb0477ae82e8ead4fe2
nanafromjeju <nanafromjeju@gmail.com>
2024-12-05 04:25:47 +0900

    design: 마이페이지 프로필 페이지 퍼블리싱 (#137)

--- app/(dashboard)/my/profile/page.tsx
+++ app/(dashboard)/my/profile/page.tsx
@@ -1,7 +1,28 @@
+import classNames from 'classnames/bind'
+
+import { LinkButton } from '@/shared/ui/link-button'
+
 import UserInfo from './_ui/user-info'
+import UserProfile from './_ui/user-profile'
+import styles from './page.module.scss'
+
+const cx = classNames.bind(styles)
 
 const MyProfilePage = () => {
-  return <UserInfo />
+  return (
+    <div className={cx('container')}>
+      <p className={cx('title')}>나의 정보</p>
+      <div className={cx('wrapper')}>
+        <UserInfo />
+        <div className={cx('user-profile')}>
+          <UserProfile userType={'트레이더'} name={'고양이'} email={'meow@example.com'} />
+          <div className={cx('link-button')}>
+            <LinkButton href={''}>탈퇴하기</LinkButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MyProfilePage
