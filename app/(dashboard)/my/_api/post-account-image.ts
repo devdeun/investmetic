96173e33e4ed546c467c667b0c6c726784e6ab97
James <dawn147@snu.ac.kr>
2024-12-05 16:58:51 +0900

    feat: 실계좌 이미지 삭제 api 연결 (#259)

--- app/(dashboard)/my/_api/post-account-image.ts
+++ app/(dashboard)/my/_api/post-account-image.ts
@@ -17,10 +17,33 @@ interface UploadAccountImagesResponseModel {
   code: number
 }
 
+interface DeleteAccountImagesRequestModel {
+  strategyId: number
+  imageIds: number[]
+}
+
+interface DeleteAccountImagesResponseModel {
+  isSuccess: boolean
+  message: string
+  code: number
+}
+
 export const uploadAccountImages = async (
   strategyId: number,
   data: UploadAccountImagesRequestModel[]
 ): Promise<UploadAccountImagesResponseModel> => {
   const response = await axiosInstance.post(`/api/my-strategies/${strategyId}/account-images`, data)
+  return response.data
+}
+
+export const deleteAccountImages = async ({
+  strategyId,
+  imageIds,
+}: DeleteAccountImagesRequestModel): Promise<DeleteAccountImagesResponseModel> => {
+  const response = await axiosInstance.post(
+    `/api/my-strategies/${strategyId}/delete-account-images`,
+    imageIds
+  )
+  console.log(response.data)
   return response.data
 }
