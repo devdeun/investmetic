0a115f268de0e965a37d1b00606b82b25fcf4f8a
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-12-11 18:03:13 +0900

    fix: 관리자 매매 유형 페이지 표의 No. index로 수정 (#83)

--- app/admin/category/_ui/shared/manage-table/utils/add-index-to-data.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react'
-
-const addIndexAndButton = (data: (string | number | ReactNode)[][]) => {
-  return data?.map((d, idx) => [idx + 1, ...d])
-}
-
-export default addIndexAndButton
