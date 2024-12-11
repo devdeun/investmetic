1518bafb44c0a5e0dbbf165a6e590ac240de51bc
ssumanlife <ksm9805@naver.com>
2024-12-11 18:40:23 +0900

    bug: 파일 다운로드시 파일명, 확장자 명시되도록 수정 (#87)

--- app/(dashboard)/strategies/[strategyId]/_api/get-proposal-download.ts
+++ app/(dashboard)/strategies/[strategyId]/_api/get-proposal-download.ts
@@ -1,18 +1,23 @@
+import { AxiosError } from 'axios'
+
 import axiosInstance from '@/shared/api/axios'
 
-import { downloadFile } from './helper-download-file'
+import { downloadFile, extractFileName } from './helper-download-file'
 
 const getProposalDownload = async (strategyId: number, name: string) => {
   try {
     const response = await axiosInstance.get(`/api/my-strategies/${strategyId}/download-proposal`, {
       responseType: 'blob',
     })
-    const blob = new Blob([response.data], { type: response.headers['content-type'] })
+    const mimeType = response.headers['content-type'] || 'application/octet-stream'
+    const blob = new Blob([response.data], { type: mimeType })
     const contentDisposition = response.headers['content-disposition']
 
-    downloadFile(blob, contentDisposition, `${name}_제안서`)
+    const fileName = extractFileName(contentDisposition, `${name}_제안서`)
+
+    downloadFile(blob, fileName)
   } catch (err) {
-    console.error(err)
+    throw new Error('분석자료 다운 실패', err as AxiosError)
   }
 }
 
