c7a905aaecc147b43949320a8e343cf57a585244
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-12-12 03:41:06 +0900

    feat: 관리자 문의 내역 페이지에 alert modal 작용 (#105)

--- app/admin/questions/_ui/admin-question-delete-button/index.tsx
+++ app/admin/questions/_ui/admin-question-delete-button/index.tsx
@@ -2,7 +2,9 @@
 
 import classNames from 'classnames/bind'
 
+import useModal from '@/shared/hooks/custom/use-modal'
 import { Button } from '@/shared/ui/button'
+import AlertModal from '@/shared/ui/modal/alert-modal'
 
 import useDeleteQuestion from '../../_hooks/query/use-delete-question'
 import styles from './styles.module.scss'
@@ -14,18 +16,29 @@ interface Props {
 }
 const AdminQuestionDeleteButton = ({ questionId, strategyId }: Props) => {
   const { mutate, isPending } = useDeleteQuestion({ strategyId, questionId })
-  const onClick = () => mutate()
+  const { closeModal, isModalOpen, openModal } = useModal()
+
   return (
-    <Button
-      variant="filled"
-      onClick={onClick}
-      disabled={isPending}
-      size="small"
-      style={{ padding: '7px 16px' }}
-      className={cx('button')}
-    >
-      삭제
-    </Button>
+    <>
+      <Button
+        variant="filled"
+        onClick={openModal}
+        size="small"
+        style={{ padding: '7px 16px' }}
+        className={cx('button')}
+      >
+        삭제
+      </Button>
+      <AlertModal
+        message={`해당 문의내역을\n삭제하시겠습니까?`}
+        isModalOpen={isModalOpen}
+        disabled={isPending}
+        onCancel={closeModal}
+        onConfirm={() => {
+          mutate()
+        }}
+      />
+    </>
   )
 }
 export default AdminQuestionDeleteButton
